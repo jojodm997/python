5779d1d286bee31d94c8b1eb428a40fe5b975765
Fauzan <fauzandwimulyono@gmail.com>
2025-02-02 17:57:21 +0700

    Hot change

--- list.py
+++ list.py
@@ -1,21 +1,21 @@
 supplies = ["Tent", "sleeping bags", "water", "raspberry pi", "coffe", "knife", "ethernet cable", "flash drive", "beard oil", "marshmallows", ]
 
 camp_site = ["Crystal Lake", 404, 89.3, True]
-#me = camping_list[4]
-#you = camping_list[-9]
-#print(me)
-#print(you)
-#print(camping_list)
+me = supplies[4]
+you = supplies[-9]
+print(me)
+print(you)
+print(supplies)
 
 supplies.extend(["toilet pepper", "bidet"])
 
-#supplies = supplies + ["toilet pepper", "bidet"]
+supplies = supplies + ["toilet pepper", "bidet"]
 
-#supplies.insert(0, "popok")
+supplies.insert(0, "popok")
 
-#supplies.insert(-1, "pengangguran")
+supplies.insert(-1, "pengangguran")
 
-#supplies.clear()
+supplies.clear()
 
 atuple = ("fauzan", "hackwell",)
 
